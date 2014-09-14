1487baa53918ec88b5d886c29c5961801660731e
Frahaan Hussain <Sonar Systems>
2014-09-14 13:29:08 +0100

    Setting Up For Android On Mac

---
+++ TutorialCCJSAndroid/frameworks/js-bindings/cocos2d-x/cocos/platform/android/java/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/sonarsystems/Desktop/TutorialCCJSAndroid/frameworks/js-bindings/cocos2d-x/cocos/platform/android/java/bin/AndroidManifest.xml : \
+/Users/sonarsystems/Desktop/TutorialCCJSAndroid/frameworks/js-bindings/cocos2d-x/cocos/platform/android/java/AndroidManifest.xml \
---
+++ TutorialCCJSAndroid/frameworks/js-bindings/cocos2d-x/cocos/platform/android/java/gen/R.java.d
@@ -0,0 +1,1 @@
+ : /Users/sonarsystems/Desktop/TutorialCCJSAndroid/frameworks/js-bindings/cocos2d-x/cocos/platform/android/java/bin/AndroidManifest.xml \
